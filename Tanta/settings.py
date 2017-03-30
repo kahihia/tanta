e2fcbbfa2977bba0538770f0ca875b0618a2dd78
jamestthompson3 <jamestthompson3@gmail.com>
2017-03-30 13:47:57 -0700

    working on setting up models

--- Tanta/settings.py
+++ Tanta/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    STATIC_dir,
-
+    STATIC_dir
+    
 ]
--- Tanta/urls.py
+++ Tanta/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from home_page import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns =[
     url(r'^admin/', admin.site.urls),
     url(r'^',include("home_page.urls",namespace='home_page')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
