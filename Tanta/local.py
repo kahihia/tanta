bcc27b0a67918b51807fa562009c910bfbb56ce9
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-11 14:45:37 -0700

    Finally fixed production problem

--- Tanta/local.py
+++ Tanta/local.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'dashboard',
     'community',
     'wallet',
+    'actstream',
 ]
 
 MIDDLEWARE = [
--- Tanta/production.py
+++ Tanta/production.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'dashboard',
     'community',
     'wallet',
+    'actstream',
 ]
 
 MIDDLEWARE = [
