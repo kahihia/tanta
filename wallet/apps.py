5f7206caf6e7b0a1222a695dcee08d0aa2337a5e
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-11 14:40:09 -0700

    trying to figure out why deployment isn't working'

--- wallet/apps.py
+++ wallet/apps.py
@@ -3,8 +3,8 @@ from django.apps import AppConfig
 class WalletConfig(AppConfig):
     name = 'wallet'
 
-    def ready(self):
-    	from actstream import registry
-    	registry.register(self.get_model('Wallet'))
-    	registry.register(self.get_model('Transactions'))
-    	# registry.register(self.get_model('User'))
+    # def ready(self):
+    # 	from actstream import registry
+    # 	registry.register(self.get_model('Wallet'))
+    # 	registry.register(self.get_model('Transactions'))
+    # 	# registry.register(self.get_model('User'))
