d05c3daafaa88ad08baea846eaf40d10d1aea3f7
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-11 14:34:41 -0700

    adjusting production settings

--- Tanta/local.py
+++ Tanta/local.py
@@ -30,7 +30,12 @@ SECRET_KEY = '%%qp$f0@*3(vk4m4l8@ao@et8g+$3vzt^b_aw0(zyns0nm*vbh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CSRF_COOKIE_SECURE=True
+SESSION_COOKIE_SECURE=True
+SECURE_HSTS_SECONDS=30
+SECURE_CONTENT_TYPE_NOSNIFF=True
+SECURE_BROWSER_XSS_FILTER=True
+X_FRAME_OPTIONS='DENY'
 ALLOWED_HOSTS = ["get-onyx.herokuapp.com",'localhost']
 
 
