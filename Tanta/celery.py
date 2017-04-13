bb3edb5860d5ce32730959be573914a631ed252e
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-13 15:05:44 -0700

    working on production to run celery

--- Tanta/celery.py
+++ Tanta/celery.py
@@ -8,7 +8,7 @@ app = Celery('Tanta')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
