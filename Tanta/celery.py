fbd14b729335cb5b2bc7393b122d7595083f5906
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-13 15:22:17 -0700

    trying to adjust settings so that celery will work. Also trying to figure out login issues on productions server

--- Tanta/celery.py
+++ Tanta/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import,unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tanta.production')
@@ -9,7 +10,7 @@ app = Celery('Tanta')
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
