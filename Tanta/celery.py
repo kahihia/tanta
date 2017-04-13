fb2fd7039a124c96a851fe7772761c65e0d27778
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-13 15:07:23 -0700

    working on production to run celery

--- Tanta/celery.py
+++ Tanta/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tanta.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tanta.production')
 app = Celery('Tanta')
 
 # Using a string here means the worker will not have to
