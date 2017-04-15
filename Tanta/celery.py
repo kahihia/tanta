4e1de12d075f920b1e46d6e910a2b1bb0ac0e750
jamestthompson3 <jamestthompson3@gmail.com>
2017-04-14 17:11:04 -0700

    fixed prod error, celery was misconfigured

--- Tanta/celery.py
+++ Tanta/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tanta.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tanta.production')
 app = Celery('Tanta')
 
 # Using a string here means the worker will not have to
